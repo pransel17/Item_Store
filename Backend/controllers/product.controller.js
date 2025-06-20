952516d75e9add4171f030c825b6d44c82ca246e
Rosalie Sarayan <lorrainesarayan@gmail.com>
2025-06-21 00:24:35 +0800

    fix: backend part was done, controller folder for product related logic was added, and I started frontend by installing Vite to work with framework react huhu, please be nice to me eeme

---
+++ Backend/controllers/product.controller.js
@@ -0,0 +1,75 @@
+import Product from "../models/product.model.js";
+import mongoose from "mongoose";
+
+// methods 
+
+
+
+
+
+export const getProducts = async (req,res) => {
+
+    try{
+      const products = await Product.find({});
+      res.status(200).json({ success: true, data: products});
+    } catch(error){
+      console.log("Error in fetching product: ", error.message);
+      res.status(500).json({ sucess: false, message: "Server Error"});
+    }
+}
+
+
+
+export const createProduct = async (req, res) => {
+  const product = req.body; // user will send this data
+
+  if (!product.name || !product.price || !product.image){
+    return res.status(400).json({ sucess:false, message: "Please provide all fields"});
+  }
+  const newProduct = new Product(product)
+  try{
+    await newProduct.save();
+    res.status(201).json({sucess: true, data: newProduct});
+  } catch(error){
+    console.error("Error in Create product: ", error.message)
+    res.status(500).json({sucess: false, message: "Server Error"});
+  }   
+}
+
+
+
+export const updateProduct =  async (req,res) =>{
+    const { id } = req.params;
+    const product = req.body;
+  
+    if (!mongoose.Types.ObjectId.isValid(id)){
+      return res.status(404).json({ sucess: false, message: "Invalid Product Id"});
+    }
+                
+    try{
+      const updatedProduct = await Product.findOneAndUpdate({ _id: id }, product, { new: true });
+      res.status(200).json({ success: true, data: updatedProduct});
+    } catch (error){
+      res.status(500).json({ success: false, message: "Server error"});
+    }
+}
+
+
+
+export const deleteProduct = async (req,res) => {
+    const { id } = req.params;
+
+      
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({ sucess: false, message: "Invalid Product Id"});
+      }
+           
+  
+    try{
+      await Product.findByIdAndDelete(id);
+      res.status(200).json({success: true, message: "Product deleted" });
+    } catch (error) {
+      console.log("Error in deleting product: ", error.message);
+      res.status(500).json({success: false, message: "Server Error" });
+    }
+}
--- Backend/server.js
+++ Backend/server.js
@@ -6,18 +6,20 @@ import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // allow to accept json data in the req body
 
-app.use("api/proucts", productRoutes);
+app.use("/api/products/", productRoutes);
 
-
-app.listen(3000, () => {
+ 
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:3000");
+  console.log("Server started at http://localhost:" + PORT);
 });
 
 
 // username: lorrainesarayan
-//PASSWORD: VQ2d6bC71FsEqJB8
+//PASSWORD: VQ2d6bC71FsEqJB8b 
