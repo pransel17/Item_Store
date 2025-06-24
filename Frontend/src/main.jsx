45cbd82b8c22cf7fc6051b3b6f058e164e2d31c2
Francelle Sarayan <lorrainesarayan@gmail.com>
2025-06-24 17:38:56 +0800

    Nasimulan frontend tas 3hrs nagdebugging 😶‍🌫️

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // ✅ Add this!
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
 )
