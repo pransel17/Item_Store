1e53fd02903979c9a89d2b17e145542637fa7949
Francelle Sarayan <lorrainesarayan@gmail.com>
2025-06-25 16:11:55 +0800

    Fix: create product page Ui donee

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import { Button, Container, Flex, HStack, Text, useColorMode,  } from '@chakra-ui/react'
+import { Link } from "react-router-dom"; // a react router link, changes url w/ reloading page unlike html faster siya
+import { CiSquarePlus } from "react-icons/ci";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
+
 
 const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  
   return (
-    <div>Navbar</div>
+
+    <Container maxW= {"1140px"} px={4} >
+
+      <Flex 
+        h={16} 
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row"
+        }}
+        >
+          
+          <Text
+            bgGradient='linear(to-r, cyan.400, blue.500)'
+            bgClip='text' // gradien sa text
+            fontSize={{base: 22 , sm: 28 }} // base = normal screen, sm = smallscreen to make it responsive 
+            fontWeight='bold'
+            textTransform={"uppercase"}
+            textAlign={"center"}
+          >
+
+            <Link to={"/"} > Product Store 🛒 </Link>
+
+          </Text>
+
+          <HStack spacing={2} alignItems={"center"}>
+            <Link to={"/create"}>
+              <Button>
+                <CiSquarePlus fontSize={20} />
+              </Button>
+            </Link>
+            <Button onClick={ toggleColorMode }>
+              {colorMode === "light" ? <IoMoon /> : <LuSun size="20"/>}
+
+            </Button>
+
+
+
+
+          </HStack>
+         
+        
+
+
+      </Flex>
+    
+    
+    </Container>
+    
   )
 }
 
--- Frontend/src/pages/CreatePage.jsx
+++ Frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import { Container, Heading, Box, useColorModeValue, VStack, Input, Button } from "@chakra-ui/react"
+import { useState } from "react"
 
 const CreatePage = () => {
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  })
+
+
+
+  const handleAddProdcut = () => {
+    console.log(newProduct);
+  }
+
+
+  // UI
   return (
-    <div>CreatePage</div>
+    <Container maxW={"container.sm"}> 
+      <VStack spacing= {8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8} >
+          Create new Product
+        </Heading>
+    
+        <Box
+          w={"full"} bg={useColorModeValue("white", "gray.800")}
+          p={6} rounded={"lg"} shadow={"md"}
+        >
+          <VStack spacing={4}>
+            <Input 
+              placeholder={"Product Name"}
+              name="name"
+              value={newProduct.name}
+              onChange={(e) => setNewProduct({ ...newProduct, name: e.targetvalue})}
+  
+            />
+  
+            <Input 
+              placeholder={"Price"}
+              name="price"
+              type="number"
+              value={newProduct.price}
+              onChange={(e) => setNewProduct({ ...newProduct, price: e.targetvalue})}
+  
+            />
+  
+            <Input 
+              placeholder={"Image URL"}
+              name="image"
+              type="number"
+              value={newProduct.image}
+              onChange={(e) => setNewProduct({ ...newProduct, image: e.targetvalue})}
+            />
+
+            <Button colorScheme="blue" onClick={handleAddProdcut} w={"full"}>
+              Add Product
+            </Button>
+
+
+
+          </VStack>
+  
+        </Box>
+      </VStack>
+    </Container>
   )
 }
 
